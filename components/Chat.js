75264c8e942fcd9366ab675a552e5e237f3d0b5a
je-man <jennmanay@gmail.com>
2021-02-25 23:26:52 -0500

    revised

--- components/Chat.js
+++ components/Chat.js
@@ -56,7 +56,7 @@ export default class Chat extends React.Component {
      // stop listening to authentication
      this.authUnsubscribe();
      // stop listening for changes
-     this.unsubscribeListUser();
+     this.unsubscribe();
 
   }
 
